77f398a27fdd3b1ec682a49c56c8935bb5082ccb
Víctor <victor.andevert@gmail.com>
2017-07-03 10:02:25 +0200

    GREEN: user is not logged in

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,26 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
 public class TripServiceTest {
 	
+	@Test(expected=UserNotLoggedInException.class)
+	public void should_validate_logged_in_user() {
+		User user = new User();
+		TripService tripService = new StubTripService();
+		tripService.getTripsByUser(user);
+	}
+
+	private class StubTripService extends TripService{
+
+		@Override
+		protected User getLoggedUser() {
+			return null;
+		}
+		
+	}
 }
+
+
