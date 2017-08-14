2174872c51c28135154bab46ddac7b8c266aa933
Víctor <victor.andevert@gmail.com>
2017-08-14 13:20:47 +0200

    REFACTOR: inverted condition with a guard clause

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -10,17 +10,17 @@ import org.craftedsw.tripservicekata.user.UserSession;
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
 		User loggedInUser = getLoggedUser();
-		
-		if (loggedInUser != null) {
-			if (user.isFriendWith(loggedInUser)) {
-				tripList = findTripsBy(user);
-			}
-			return tripList;
-		} else {
+		if (loggedInUser == null) {
 			throw new UserNotLoggedInException();
 		}
+		
+		List<Trip> tripList = new ArrayList<Trip>();		
+		
+		if (user.isFriendWith(loggedInUser)) {
+			tripList = findTripsBy(user);
+		}
+		return tripList;
 	}
 
 	protected List<Trip> findTripsBy(User user) {
