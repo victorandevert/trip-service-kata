454e0dbec8c416d98f7efbb37052a365505638b6
Victor Andevert <victor.andevert@gmail.com>
2021-02-05 18:16:42 +0100

    add test for registered user without friends

--- src/test/kotlin/tripservicekata/trip/TripServiceShould.kt
+++ src/test/kotlin/tripservicekata/trip/TripServiceShould.kt
@@ -5,29 +5,37 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException
 import org.craftedsw.tripservicekata.user.User
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.util.ArrayList
 
 class TripServiceShould {
 
-
     @Test
     fun `throw and exception when user is not logged in`() {
+        val anonymousUser = null
+        val user = User()
+
+        val tripService = TripServiceMock(anonymousUser)
+
         assertThrows<UserNotLoggedInException> {
-            val tripService = TripServiceMock()
-            val user = User()
             tripService.getTripsByUser(user)
         }
     }
 
     @Test
     fun `not return trips when the user is logged in and has no friends`() {
+        val registeredUser = User()
+        val user = User()
+        val tripService = TripServiceMock(registeredUser)
 
-        Assertions.assertThat().isEqualTo()
+        val trips = tripService.getTripsByUser(user)
+
+        Assertions.assertThat(trips).isEqualTo(ArrayList<Trip>())
     }
 
-    class TripServiceMock: TripService() {
+    class TripServiceMock(private val user: User?) : TripService() {
 
         override fun getLoggedUser(): User? {
-            return null
+            return user
         }
     }
 }
